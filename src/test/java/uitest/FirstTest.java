87ca9525081eb1fbde90af061e4fb2fff2b69603
QAankur <ankur.mishra671@gmail.com>
2021-12-08 15:16:15 +0530

    second commit

--- src/test/java/uitest/FirstTest.java
+++ src/test/java/uitest/FirstTest.java
@@ -1,12 +1,15 @@
 package uitest;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class FirstTest {
 
+	@Parameters({"Browser"})
 	@Test
-	public void m1()
+	public void m1(String browser)
 	{
+		System.out.println("Passed value for parameters is" + browser);
 		System.out.println("Jenkins is up and running");
 	}
 }
